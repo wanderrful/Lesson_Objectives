a5cb007e0e6f174a144dae1766aa910793e31410
Wanderrful <wanderrful@gmail.com>
2012-04-19 14:50:58 +0900

    Added lesson_objectives.rb

---
+++ lesson_objectives.rb
@@ -0,0 +1,161 @@
+# g7.txt
+# g8.txt
+# g9.txt
+
+g7a = [ "Identify variables, numerical expressions, and variable expressions.","Write variable expressions for word phrases.","Use the order of operations.","Use grouping symbols.","Evaluate variable expressions.","Solve problems by evaluating expressions.","Represent, graph, and order integers.","Find opposites and absolute values.","Use models to add integers.","Use rules to add integers.","Use models to subtract integers.","Use a rule to subtract integers.","Write rules for patterns.","Make predictions and test conjectures.","Find number patterns.","Multiply integers using repeated addition, patterns, and rules.","Divide integers using rules.","Name coordinates and quadrants in the coordinate plane.","Graph points in the coordinate plane.","Identify properties of addition and multiplication.","Use properties to solve problems.","Use the Distributive Property with numerical expressions.","Use the Distributive Property with algebraic expressions." ]
+g7b = [ "Identify parts of a variable expression.","Simplify expressions.","Classify types of equations.","Check equations using substitution.","Solve one-step equations using subtraction.","Solve one-step equations using addition.","Solve one-step equations using division.","Solve one-step equations using multiplication.","Solve a problem using the guess, check, rise strategy.","Graph inequalities.","Write inequalities.","Solve one-step inequalities using subtraction.","Solve one-step inequalities using addition.","Solve one-step inequalities using division.","Solve one-step inequalities using multiplication." ]
+g7c = [ "Round decimals.","Estimate sums and differences.","Estimate products.","Estimate quotients.","Find mean, median, and mode of a set of data.","Choose the best measure of central tendency.","Substitute into formulas.","Use the formula for the perimeter of a rectangle.","Solve one-step decimal equations involving addition.","Solve one-step decimal equations involving subtraction.","Solve one-step decimal equations involving division.","Solve one-step decimal equations involving multiplication.","Identify appropriate metric measures.","Convert metric units.","Solve problems by acting them out." ]
+g7d = [ "Use divisibility tests.","Find factors.","Use exponents.","Use the order of operations with exponents.","Find the prime factorization of a number.","Find the greatest common factor of two or more numbers.","Find equivalent fractions.","Write fractions in simplest form.","Find equivalent fractions.","Write fractions in simplest form.","Solve complex problems by first solving simpler cases.","Identify and graph rational numbers.","Evaluate fractions containing variables.","Multiply powers with the same base.","Find a power of a power.","Divide expressions containing exponents.","Simplify expressions with integer exponents.","Write and evaluate numbers in scientific notation.","Calculate with scientific notation." ]
+g7e = [ "Find the least common multiple.","Compare fractions.","Write fractions as decimals.","Write terminating and repeating decimals as fractions.","Add and subtract fractions.","Add and subtract mixed numbers.","Multiply fractions.","Divide fractions.","Identify appropriate customary units.","Convert customary units.","Solve problems by working backward.","Solve equations by subtracting fractions.","Solve equations by adding fractions.","Solve equations by multiplying fractions.","Solve equations by multiplying mixed numbers.","Find powers of products.","Find powers of quotients." ]
+g7f = [ "Write and simplify ratios.","Find rates and unit rates.","Solve proportions.","Use proportions to solve problems","Solve problems that involve similar figures.","Solve problems that involve scale drawings.","Find probability.","Find odds.","Write percents as fractions and decimals.","Write decimals and fractions as percents.","Find a part of a whole and a percent.","Find a whole amount.","Write and solve percent equations.","Use equations in solving percent problems.","Find percent of increase.","Find percent of decrease.","Find markups.","Find discounts.","Solve problems by making a table." ]
+
+g8a = [ "Model relationships with variables.","Model relationships with equations.","Simplify and evaluate expressions and formulas.","Simplify and evaluate expressions containing grouping symbols.","Classify numbers.","Compare numbers.","Write a function rule.","Understand relationships of quantities in a function.","Analyze data using scatter plots.","Find mean, median, and mode.","Make and use stem-and-leaf plots." ]
+g8b = [ "Add real numbers using models and rules.","Apply addition.","Subtract rational numbers.","Apply subtraction.","Multiply real numbers.","Divide real numbers.","Use the Distributive Property.","Simplify algebraic expressions.","Identify properties.","Use deductive reasoning.","Find theoretical probability.","Find experimental probability.","Find the probability of independent events.","Find the probability of dependent events." ]
+g8c = [ "Solve two-step equations.","Use deductive reasoning.","Use the Distributive Property when combining like terms.","Use the Distributive Property when solving equations.","Solve equations with variables on both sides.","Identify equations that are identities or have no solution.","Find ratios and rates.","Solve proportions.","Find missing measures of similar figures.","Use similar figures when measuring indirectly.","Define a variable in terms of another variable.","Model distance-rate-time problems.","Find percent of change.","Find percent error.","Find square roots.","Estimate and use square roots.","Solve problems using Pythagorean Theorem.","Identify right triangles." ]
+g8d = [ "Identify solutions of inequalities.","Graph and write inequalities.","Use addition to solve inequalities.
+Use subtraction to solve inequalities.","Use multiplication to solve inequalities.","Use division to solve inequalities.","Solve multi-step inequalities with variables on one side.","Solve multi-step inequalities with variables on both sides.","Solve and graph inequalities containing and.","Solve and graph inequalities containing or.","Solve equations that involve absolute value.","Solve inequalities that involve absolute value." ]
+g8e = [ "Interpret, sketch, and analyze graphs from situations.","Identify relations and functions.","Evaluate functions.","Model functions using rules, tables, and graphs.","Write a function rule given a table or a real-world situation.","Write an equation of a direct variation.","Use ratios and proportions with direct variations.","Solve inverse variations.","Compare direct and inverse variation.","Use inductive reasoning in continuing number patterns.","Write rules for arithmetic sequences." ]
+g8f = [ "Find rates of change from tables and graphs.","Find slopes.","Write linear equations in slope-intercept form.","Graph linear equations.","Interpret Linear Graphs.","Graph equations using intercepts.","Write equations in standard form.","Graph and write linear equations using point-slope form.","Write a linear equation using data.","Determine whether lines are parallel.","Determine whether lines are perpendicular.","Write an equation for a trend line and use it to make predictions.","Translate the graph of an absolute value equation." ]
+
+g9a = [ "Use inductive reasoning to make conjectures.","Make isometric and orthographic drawings.","Draw nets for three-dimensional figures.","Understand basic terms of geometry.","Understand basic problems of geometry.","Identify segments and rays.","Recognize parallel lines.","Find the lengths of segments.","Find the measures of angles.","Identify special angle pairs.","Use a compass and a straightedge to construct congruent segments and congruent angles.","Use a compass and a straightedge to bisect segments and angles.","Find the distance between two points in the coordinate plane.","Find the coordinates of the midpoint of a segment in the coordinate plane.","Find perimeters of rectangles and squares, and circumferences of circles.","Find areas of rectangles, squares, and circles." ]
+g9b = [ "Recognize conditional statements.","Write converses of conditional statements.","Write biconditionals.","Recognize good definitions.","Use the Law of Detachment.","Use the Law of Syllogism.","Connect reasoning in algebra with geometry.","Prove and apply theorems about angles." ]
+g9c = [ "Identify angles formed by two lines and a transversal.","Prove and use properties of parallel lines.","Use a transversal in proving lines parallel.","Relate parallel and perpendicular lines.","Classify triangles and find the measures of their angles.","Use exterior angles of triangles.","Classify polygons.","Find the sums of the measures of the interior and exterior angles of polygons.","Graph lines given their equations.","Write equations of lines.","Relate slope and parallel lines.","Relate slope and perpendicular lines.","Construct parallel lines.","Construct perpendicular lines." ]
+g9d = [ "Recognize congruent figures and their corresponding parts.","Prove two triangles congruent using the SSS and SAS Postulates.","Prove two triangles congruent using the ASA Postulate and the AAS Theorem.","Use triangle congruence and CPCTC to prove that parts of two triangles are congruent.","Use and apply properties of isosceles triangles.","Prove triangles congruent using the HL Theorem.","Identify congruent overlapping triangles.","Prove two triangles congruent by first proving two other triangles congruent." ]
+g9e = [ "Use properties of midsegments to solve problems.","Use properties of perpendicular bisectors and angle bisectors.","Identify properties of perpendicular bisectors and angle bisectors.","Identify properties of medians and altitudes of a triangle.","Write the negation of a statement and the inverse and contrapositive of a conditional statement.","Use indirect reasoning.","Use inequalities involving angles of triangles.","Use inequalities involving sides of triangles." ]
+g9f = [ "Define and classify special types of quadrilaterals.","Use relationships among sides and among angles of parallelograms.","Use relationships involving diagonals of parallelograms and transversals.","Determine whether a quadrilateral is a parallelogram.","Use properties of diagonals of rhombuses and rectangles.","Determine whether a parallelogram is a rhombus or a rectangles.","Verify and use properties of trapezoids and kites.","Name coordinates of special figures by using their properties.","Prove theorems using figures in the coordinate plane." ]
+
+g7 = "g7.txt"
+g8 = "g8.txt"
+g9 = "g9.txt"
+
+class String
+  def uncapitalize 
+    self[0, 1].downcase + self[1..-1]
+  end
+end
+
+File.open(g7,"w+") do |f|
+	f.puts "\tChapter 1:\r\n\r\n"
+	g7a.each { |g|
+		f.print "After these lessons, the students will be able to ", g.uncapitalize
+	}
+end
+
+File.open(g7,"w+") do |f|
+	f.puts "\tChapter 2:\n\n"
+	g7b.each { |g|
+		f.print "After these lessons, the students will be able to ", g.uncapitalize
+	}
+end
+
+File.open(g7,"w+") do |f|
+	f.puts "\tChapter 3:\n\n"
+	g7c.each { |g|
+		f.print "After these lessons, the students will be able to ", g.uncapitalize
+	}
+end
+
+File.open(g7,"w+") do |f|
+	f.puts "\tChapter 4:\n\n"
+	g7d.each { |g|
+		f.print "After these lessons, the students will be able to ", g.uncapitalize
+	}
+end
+
+File.open(g7,"w+") do |f|
+	f.puts "\tChapter 5:\n\n"
+	g7e.each { |g|
+		f.print "After these lessons, the students will be able to ", g.uncapitalize
+	}
+end
+
+File.open(g7,"w+") do |f|
+	f.puts "\tChapter 6:\n\n"
+	g7f.each { |g|
+		f.print "After these lessons, the students will be able to ", g.uncapitalize
+	}
+end
+
+File.open(g8,"w+") do |f|
+	f.puts "\tChapter 1:\n"
+	g8a.each { |g|
+		f.print "After these lessons, the students will be able to ", g.uncapitalize
+	}
+end
+
+File.open(g8,"w+") do |f|
+	f.puts "\tChapter 2:\n"
+	g8b.each { |g|
+		f.print "After these lessons, the students will be able to ", g.uncapitalize
+	}
+end
+
+File.open(g8,"w+") do |f|
+	f.puts "\tChapter 3:\n"
+	g8c.each { |g|
+		f.print "After these lessons, the students will be able to ", g.uncapitalize
+	}
+end
+
+File.open(g8,"w+") do |f|
+	f.puts "\tChapter 4:\n"
+	g8d.each { |g|
+		f.print "After these lessons, the students will be able to ", g.uncapitalize
+	}
+end
+
+File.open(g8,"w+") do |f|
+	f.puts "\tChapter 5:\n"
+	g8e.each { |g|
+		f.print "After these lessons, the students will be able to ", g.uncapitalize
+	}
+end
+
+File.open(g8,"w+") do |f|
+	f.puts "\tChapter 6:\n"
+	g8f.each { |g|
+		f.print "After these lessons, the students will be able to ", g.uncapitalize
+	}
+end
+
+File.open(g9,"w+") do |f|
+	f.puts "\tChapter 1:\n"
+	g9a.each { |g|
+		f.print "After these lessons, the students will be able to ", g.uncapitalize
+	}
+end
+
+File.open(g9,"w+") do |f|
+	f.puts "\tChapter 2:\n"
+	g9b.each { |g|
+		f.print "After these lessons, the students will be able to ", g.uncapitalize
+	}
+end
+
+File.open(g9,"w+") do |f|
+	f.puts "\tChapter 3:\n"
+	g9c.each { |g|
+		f.print "After these lessons, the students will be able to ", g.uncapitalize
+	}
+end
+
+File.open(g9,"w+") do |f|
+	f.puts "\tChapter 4:\n"
+	g9d.each { |g|
+		f.print "After these lessons, the students will be able to ", g.uncapitalize
+	}
+end
+
+File.open(g9,"w+") do |f|
+	f.puts "\tChapter 5:\n"
+	g9e.each { |g|
+		f.print "After these lessons, the students will be able to ", g.uncapitalize
+	}
+end
+
+File.open(g9,"w+") do |f|
+	f.puts "\tChapter 6:\n"
+	g9f.each { |g|
+		f.print "After these lessons, the students will be able to ", g.uncapitalize
+	}
+end
